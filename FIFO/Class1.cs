2101fbc61b0924eab51ae2d9a82b69fcc23e029b
Muhammed Zaghloul Sherif <mohamed.zaghloul.sherif@gmail.com>
2025-04-04 17:02:01 +0200

    two classes, the first one 'Employee' is private constructor class with factory method and the second one 'EmployeeManagement' for handling and organizing the empolyees info using the first class

---
+++ FIFO/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace FIFO1
+{
+    class Employee
+    {
+        private char gender;
+        private Employee(int id,string name,char gender,decimal totalSales)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Gender = gender;
+            this.TotalSales = totalSales;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public char Gender { 
+            get{
+                return gender;
+
+            }
+            set
+            {
+                if (value == 'm' || value == 'f' || value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    throw new ArgumentException("Unkown Gender");
+            }
+        }
+        public decimal TotalSales { get; set; }
+
+        public static Employee newEmployee(int id,string name,char gender,decimal totalSales)
+        {
+            return new Employee(id,name,gender,totalSales);
+        }
+    }
+
+    class EmployeesManagement
+    {
+        private List<Employee> employees;
+        public EmployeesManagement()
+        {   
+            employees = new List<Employee>();
+        }
+        public void AddEmployee(int id, string name, char gender, decimal totalSales) => employees.Add(Employee.newEmployee(id,name,gender,totalSales));
+        public void PrintEmpolyees() {
+            foreach (Employee em in employees)
+            {
+                Console.WriteLine($"ID: {em.Id}, Name: {em.Name}, Total Sales:{em.TotalSales}");
+            }
+        }
+
+    }
+}
---
+++ FIFO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FIFO1
+{
+    class Program
+    {
+        private static void Main(string[]args)
+        {
+              
+            var em=new EmployeesManagement();
+
+            em.AddEmployee(1,"Muhammed",'m',15000);
+            em.AddEmployee(2, "Ahmed", 'm', 13000);
+            em.AddEmployee(3, "Mahmoud", 'm', 1000);
+            em.PrintEmpolyees();
+
+
+
+        }
+    } 
+
+}
